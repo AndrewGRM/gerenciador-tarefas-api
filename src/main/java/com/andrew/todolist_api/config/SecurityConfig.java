e3e47ef14b96bb8ccd3ddab35cc32c718a6247cc
AndrewGRM <andrew.grm15@gmail.com>
2025-08-14 16:47:53 -0300

    #

--- src/main/java/com/andrew/todolist_api/config/SecurityConfig.java
+++ src/main/java/com/andrew/todolist_api/config/SecurityConfig.java
@@ -3,14 +3,13 @@ package com.andrew.todolist_api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -22,10 +21,8 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
                         // permite o acesso ao console do H2 sem autentificação
-                        .requestMatchers("/h2-console/**").permitAll()
-                        // Permite acesso à documentação do Springdoc OpenAPI (Swagger UI e definição da API)
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**" ).permitAll()
-                        // Regras de ADMIN (as mais específicas primeiro)
+                        .requestMatchers("/h2-console/**").hasRole("ADMIN")
+                        // Regras de ADMIN
                         .requestMatchers(HttpMethod.POST, "/tasks").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/tasks/{id}").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/tasks/{id}").hasRole("ADMIN")
@@ -33,6 +30,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/tasks").hasAnyRole("USER", "ADMIN")
                         .requestMatchers(HttpMethod.GET, "/tasks/{id}").hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/v3/api-docs/**").permitAll()
+                        // Permite acesso à documentação do Springdoc OpenAPI (Swagger UI e definição da API)
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**" ).hasAnyRole("User", "ADMIN")
                         // Qualquer outra requisição deve ser autenticada
                         .anyRequest().authenticated())
 
